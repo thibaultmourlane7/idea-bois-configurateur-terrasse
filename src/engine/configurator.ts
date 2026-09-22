e059f8c2f95d727cb048cc6b6237ba88626e42f6
thibaultmourlane7 <thibaultmourlane7@gmail.com>
2026-09-22 17:36:01 +0200

    Phase 2 version 0.16.3

--- src/engine/configurator.ts
+++ src/engine/configurator.ts
@@ -11,7 +11,7 @@ import { computeTechnicalSizing } from './technical';
 import { computeStructure } from './structure';
 import { computeSupportPlan, SUPPORT_PLAN_TAG } from './supportPlan';
 
-export const VERSION_TAG = 'IB-TERR-VERSION-016.2';
+export const VERSION_TAG = 'IB-TERR-VERSION-016.3';
 export const CATALOG_TAG = 'SA-TERR-CATALOG-002';
 export const GAP_TAG = 'SA-TERR-GAP-001';
 
